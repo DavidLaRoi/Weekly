6ed77c8791eaad955f47a751c9f1ffdde41be532
David la Roi <David.la.Roi@Logic4.nl>
2021-06-24 21:00:07 +0200

    a

--- Weekly.Behaviour/RelayCommand.cs
+++ Weekly.Behaviour/RelayCommand.cs
@@ -1,4 +1,34 @@
-﻿namespace Weekly.Behaviour
-{
+﻿using System;
+using System.Windows.Input;
 
+namespace Weekly.Behaviour
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
